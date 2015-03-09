3180351c37146cda446ff3f7c7306719f470d851
kumar <arunkumar@impelcrm.in>
2015-03-09 18:33:10 +0530

    first commit

---
+++ LoadMonitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+r=$(cat /proc/loadavg | awk -F. '{print $1}')
+if [ $r -ge 6 ]
+then
+/bin/mail -s "Warning: High Server Load on: `droplet.impelcrm.in` Load Average: $r"   arunkumar@impelcrm.in
+fi
---
+++ README.md
@@ -0,0 +1,1 @@
+git testing page for githup
